a01444f868bccf3b2c31601df2dd2b352af0c436
ommit <ommit.shah@gmail.com>
2015-08-30 16:13:14 +0300

    Async ls

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,11 +1,20 @@
 var Q = require('q'),
   _ = require('lodash'),
-  fs = require('fs');
+  fs = require('fs'),
+  util = require('util');
 
-fs.readFile(process.argv[2], 'utf-8',function(err, data) {
-	console.log(data.toString().split('\n').length - 1);
-});
-/*Q.nfcall(fs.readFile, process.argv[2], 'utf-8')
-.then(function(buf){
-  console.log(buf.split('\n').length - 1);
-});*/
+var dir = process.argv[2];
+var ext = process.argv[3];
+Q.nfcall(fs.readdir, dir)
+.then(function(files) {
+	_(files)
+	.filter(function(f) { return f.indexOf(ext) >0;})
+	.forEach(function(d) {
+		console.log(d);
+	}).value();
+})
+.done();
+
+fs.readdir(dir, function(data) {
+	;
+})
