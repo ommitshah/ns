fbeb71ecb03fdab9d46c80d4f752e01207af2103
ommit <ommit.shah@gmail.com>
2015-08-30 18:57:50 +0300

    multibuf using q-io

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,6 +1,10 @@
-var http = require('http');
-
-http.get(process.argv[2], function(response) {
-	response.setEncoding('utf-8');
-	response.on('data', console.log);
-});
+var HTTP = require('q-io/http'), util = require('util');
+HTTP.request(process.argv[2])
+.then(function(res){
+	return res.body.read();
+})
+.then(function(body){
+	console.log(body.length)
+	console.log(body.toString());
+})
+.done();
