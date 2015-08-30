4a7b1d7156b47cf94106778bee8de5f757f3f0f8
ommit <ommit.shah@gmail.com>
2015-08-30 18:44:51 +0300

    q-io + http

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,15 +1,6 @@
-var module = require('./module'),
-  _ = require('lodash');
+var http = require('http');
 
-var dir = process.argv[2];
-var ext = process.argv[3];
-
-function cb(err, data) {
-	if (err) {
-		console.log(err);
-	} else {
-	  _(data).forEach(function(d) {console.log(d);}).value();
-	}
-}
-
-module(dir,ext, cb);
+http.get(process.argv[2], function(response) {
+	response.setEncoding('utf-8');
+	response.on('data', console.log);
+});
