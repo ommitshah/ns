c4fe451fed68c0d001a19cdcef29956ba034e249
ommit <ommit.shah@gmail.com>
2015-08-30 14:16:06 +0300

    Async IO

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -2,9 +2,10 @@ var Q = require('q'),
   _ = require('lodash'),
   fs = require('fs');
 
-var buf = fs.readFileSync(process.argv[2], 'utf-8');
-console.log(buf.split('\n').length -1);
-/*Q.fcall(fs.readFileSync, process.argv[2], 'utf-8')
+fs.readFile(process.argv[2], 'utf-8',function(err, data) {
+	console.log(data.toString().split('\n').length - 1);
+});
+/*Q.nfcall(fs.readFile, process.argv[2], 'utf-8')
 .then(function(buf){
   console.log(buf.split('\n').length - 1);
 });*/
