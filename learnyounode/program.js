de5c352a6f48ba912f653722fc039781a4871a1d
ommit <ommit.shah@gmail.com>
2015-09-05 20:41:52 +0300

    3 left

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,21 +1,18 @@
 var HTTP = require('q-io/http'),
   Q = require('q'),
   util = require('util'),
-  _ = require('lodash');
+  _ = require('lodash'),
+  net = require('net');
+  strftime = require('strftime');
 
-var urls = _(process.argv).slice(2).value();
-var promises = _(urls).map(function (url) {
-  return HTTP.request(url);
-}).value();
+var port = parseInt(process.argv[2]);
 
-Q.all(promises)
-  .then(function(responses) {
-    return _(responses).map(function(res) { return res.body.read();}).value();
-  })
-  .then(Q.all)
-  .then(function(bodies) {
-    bodies.forEach(function(body) {
-      console.log(body.toString());
-    });
-  })
-  .done();
+var server = net.createServer(function(socket) {
+  socket.end(now());
+});
+
+server.listen(port);
+function now() {
+  return strftime('%Y-%m-%d %H:%M')+ '\n';
+}
+
